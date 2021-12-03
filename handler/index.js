b5c5f348137785996437aafdb3a89d60030d41fe
Zikri <zikrimansyursyah@gmail.com>
2021-12-03 22:12:59 +0700

    milestone 3

--- handler/index.js
+++ handler/index.js
@@ -1,11 +1,28 @@
+require('dotenv').config()
+
 const express = require('express')
 const basicAuth = require('express-basic-auth')
 const service = require('../services')
-
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = 8000
 
-app.use((req, res, next) => {
+app.use(express.json())
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(err)
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+function login(req, res, next) {
     let name, pass;
     auth = req.headers.authorization
     const arr = atob(auth.slice(6)).split(":")
@@ -21,23 +38,26 @@ app.use((req, res, next) => {
             next()
         }
     })
-})
-app.use(express.json())
+}
 
-app.post("/login", (req, res) => {
+app.post("/login", login, (req, res) => {
     auth = req.headers.authorization
     const logData = atob(auth.slice(6)).split(":")
+    const user = { name: logData[0] }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
     res.json({
         login: 'Success',
-        username: logData[0]
+        username: logData[0],
+        accessToken: accessToken
     })
 })
-app.post("/api/merchant/regis", service.m_regis)
-app.delete("/api/merchant/delete", service.m_delete)
-app.post("/api/product/add", service.p_add)
-app.delete("/api/product/delete", service.p_delete)
-app.post("/api/product/update", service.p_update)
-app.get("/api/product/:mid", service.p_list)
+app.post("/api/merchant/regis", authenticateToken, service.m_regis)
+app.delete("/api/merchant/delete", authenticateToken, service.m_delete)
+app.post("/api/product/add", authenticateToken, service.p_add)
+app.delete("/api/product/delete", authenticateToken, service.p_delete)
+app.post("/api/product/update", authenticateToken, service.p_update)
+app.get("/api/product/:mid", authenticateToken, service.p_list)
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
